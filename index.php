cf7f64c4e751ac3e5a7c552c9a2ae107c8eba4a7
Johnviti <84483840+Johnviti@users.noreply.github.com>
2023-06-18 11:00:46 -0300

    Starting

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$x = 20;
+$nome = "John";
+$y = $x + 10;
+echo "Soma é " . $y . "<br>";
+echo "Meu nome é " . $nome . "<br>";
+echo "Isto é PHP<br>";
+?>
+
+</body>
+</html>
+
+
+
